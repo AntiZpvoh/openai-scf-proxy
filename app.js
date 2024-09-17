56df437d40af7560007e9787151b6ebaaf765017
Ubuntu <ubuntu@ip-172-31-31-33.us-west-2.compute.internal>
2024-09-17 04:05:33 +0000

    change app.js

--- app.js
+++ app.js
@@ -5,13 +5,25 @@ const {
 const app = express()
 const port = 9000
 
-app.use('/', createProxyMiddleware({
+function convertTime(timestamp){
+  var newDate = new Date();
+  newDate.setTime(timestamp*1000);
+  return newDate.toUTCString();
+}
+
+// app.use(express.json())
+// app.use(express.urlencoded({extended: true}))
+app.all('/chatgpt-proxy/*', createProxyMiddleware({
   target: 'https://api.openai.com',
   changeOrigin: true,
   onProxyReq: (proxyReq, req, res) => {
     // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
+    console.log(`[${convertTime(Date.now())}] Req URL: `, req.url);
+    console.log(`[${convertTime(Date.now())}] Req Headers from ${req.ip}: `, req.headers);
     proxyReq.removeHeader('x-forwarded-for');
     proxyReq.removeHeader('x-real-ip');
+    // console.log(`[${convertTime(Date.now())}] Req URL: `, req.originalUrl);
+    // console.log(`[${convertTime(Date.now())}] Req headers from ${req.originalUrl}: `, req.headers);
   },
   onProxyRes: function (proxyRes, req, res) {
     proxyRes.headers['Access-Control-Allow-Origin'] = '*';
